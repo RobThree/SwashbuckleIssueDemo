db79b55f126dedc99a7aa48bd8866fcd8bfd45b2
RobIII <r.janssen@keensystems.eu>
2021-09-08 11:41:02 +0200

    Change to Guid

--- SwashbuckleTest/Controllers/TestController.cs
+++ SwashbuckleTest/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using SwashbuckleTest.Infrastructure.ObjectId;
 using SwashbuckleTest.Models;
+using System;
 using System.Runtime.CompilerServices;
 
 namespace SwashbuckleTest.Controllers
@@ -18,31 +19,31 @@ namespace SwashbuckleTest.Controllers
 
         // Argument "id" is not picked up 
         [HttpGet("testgetquerystring1")]
-        public string GetQuerystring1(ObjectId id, string name)
+        public string GetQuerystring1(Guid id, string name)
             => BuildResult(id, name);
 
         // Shows "IsNew" as argument name
         [HttpGet("testgetquerystring2")]
-        public string GetQuerystring2([FromQuery] ObjectId id, string name)
+        public string GetQuerystring2([FromQuery] Guid id, string name)
             => BuildResult(id, name);
 
         // Same as above, now with name specified but is ignored
         [HttpGet("testgetquerystring3")]
-        public string GetQuerystring3([FromQuery(Name = "id")] ObjectId id, string name)
+        public string GetQuerystring3([FromQuery(Name = "id")] Guid id, string name)
             => BuildResult(id, name);
 
 
         // == These work fine ==
         [HttpGet("testgetroute/{id}")]
-        public string GetRoute(ObjectId id, [FromQuery] string name)
+        public string GetRoute(Guid id, [FromQuery] string name)
             => BuildResult(id, name);
 
         [HttpPost("testpost")]
         public string Post(TestDTO testdto)
-            => BuildResult(testdto.Id, testdto.Name);
+            => BuildResult(Guid.Empty, testdto.Name);
 
 
-        private string BuildResult(ObjectId id, string name, [CallerMemberName] string caller = "")
+        private string BuildResult(Guid id, string name, [CallerMemberName] string caller = "")
             => $"Hello, test from [{caller}]: {id}, {name}";
     }
 }
